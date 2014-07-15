4e556d388063085ed48d8af00351434a5730b457
Nick Bernard & Katharine Chen <student@epicodus.com>
2014-07-15 15:24:33 -0700

    Added New List functionality with uncompleted adding tasks functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,65 @@
-$(document).ready(function(){
-  $("form#task").submit(function(event){
+$(document).ready(function() {
+
+  event.preventDefault();
+
+  var masterList = [];
+
+  $("form#new-list").submit(function(event) {
     event.preventDefault();
+    var inputtedNewList = $("input#list-name").val();
+    var list = { listName: inputtedNewList, tasks: [] };
 
-    var inputtedList = $("input#list-name").val();
-    var inputtedTask = $("input#task-name").val();
-    var inputtedDate = $("input#due-date").val();
-    var list = { listName: inputtedList, task: [] };
-    var task = { name: inputtedTask, date: inputtedDate };
+    masterList.push(list);
 
+    $(".task-lists").show();
+    $("ul.lists").append("<li class='task-list'>" +
+                        list.listName + "</li>");
+    $("input#list-name").val("");
 
-    if (inputtedTask != "") {
-      $("tbody.tasks").append("<tr>" + "<td class='tasks'>" + task.name + "</td>" + "<td class='due'>" + task.date + "</td>" + "<td>" + "<input type='checkbox' class='checkbox'>" + "</td>" + "</tr>");
-    } else {
-      alert("Please enter a task and a due date!");
-    }
+    $(".task-list").last().click(function() {
+      var currentList = list;
+      $("#individual-results h2").text(currentList.listName);
+      $("#individual-results").show();
 
+      $("form#new-task").submit(function(event) {
 
-    $("input#task-name").val("");
-    $("input#due-date").val("");
+        var taskNew = $("input#task").val();
 
-    $("input.checkbox").click(function() {
-      $(this).closest("tr").empty();
+        currentList.tasks.push(taskNew);
+        console.log(currentList);
+
+        // currentList.each(function(task) {
+        // $("#task-list").append("<li>" + task + "</li>");
+        event.preventDefault();
+      });
+
     });
   });
-
-  $("button#clear").click(function(){
-    $("tbody.tasks").empty();
-  });
-
 });
+
+
+ //  $("form.new-task").submit(function(event){
+ //    var inputtedTask = $("input#task-name").val();
+ //    var task = { name: inputtedTask };
+ //    // list.last.push(task);
+
+ //    if (inputtedTask != "") {
+ //      $("table").show();
+ //      $("tbody.tasks").append("<tr>" + "<td class='tasks'>" + task.name + "</td>" + "<td class='due'>" + task.date + "</td>" + "<td>" + "<input type='checkbox' class='checkbox'>" + "</td>" + "</tr>");
+ //    } else {
+ //      alert("Please enter a task and a due date!");
+ //    }
+
+ //    $("input#task-name").val("");
+ //     event.preventDefault();
+ //  });
+ // event.preventDefault();
+
+
+// $("button#clear").click(function(){
+//   $("tbody.tasks").empty();
+// });
+
+    // $("input.checkbox").click(function() {
+    //   $(this).closest("tr").empty();
+    // });
